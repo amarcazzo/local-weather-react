5fd1e8f9e567ed1f391581e470449dccebd6645e
Ayrton Marcazzó <ayrtonmarcazzo@gmail.com>
2018-04-16 16:48:52 -0300

    Delete config.js

--- src/config.js
+++
@@ -1,6 +0,0 @@
-export const config = {
-  apiKey: 'Rz1Vca78MUakptd05ZoGIokmoSFZ2H6E',
-  locationUrl: 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search',
-  currentUrl: 'https://dataservice.accuweather.com/currentconditions/v1/',
-  fiveDaysUrl: 'https://dataservice.accuweather.com/forecasts/v1/daily/5day/'
-}
